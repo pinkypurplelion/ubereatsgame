faabaa291753819cf89f2c1ac2f05b356f9f50aa
Jayath Gunawardena <bobshen07@gmail.com>
2022-08-11 16:22:11 +1000

    Added Road Tiles
    Updated the Road Tiles with 4 different tiles, and ensured that they had the correct logic behind each of them

--- Assets/Scenes/MainGameWorld/Scripts/RoadTile.cs
+++ Assets/Scenes/MainGameWorld/Scripts/RoadTile.cs
@@ -12,32 +12,145 @@ namespace Scenes.MainGameWorld.Scripts
         **/
         public Tile Tile;
         public TileType TileType;
-        public List<Tile> Tiles { get; } = new();
-
+        public CityBlock CityBlock;
+        public Dictionary<string, bool> directions = new Dictionary<string, bool>();
         // Start is called before the first frame update
         void Start()
         {
 
-            foreach (var tile in Tiles)
+            foreach (var connection in Tile.Connections)
             {
-                if (tile.Type == TileType.Road)
+                if(connection.ConnectedTile.Type == TileType.Road)
                 {
-                    int[,] directions = new int[4, 2];
-                    int count = 0;
-                    foreach (var link in tile.Connections)
+                    if (connection.ConnectedTile.X == Tile.X)
                     {
-
-
-                        if (link.ConnectedTile.Type == TileType.Road)
+                        if (connection.ConnectedTile.Y == Tile.Y + 1)
                         {
-
-                            directions[count, 0] = link.ConnectedTile.X;
-                            directions[count, 1] = link.ConnectedTile.Y;
-
+                            directions.Add("north", true);
                         }
+                        else { directions.Add("south", true); }
+                    }
+
+                    else
+                    {
+                        if (connection.ConnectedTile.X == Tile.X + 1)
+                        { directions.Add("east", true); }
+                        else { directions.Add("west", true); }
+
                     }
                 }
+           
+          
+            }
 
+            switch (directions.Count)
+            {
+                case 0:
+                    
+                    break;
+                case 1:
+                    //Return the straight block
+                    transform.Find("4wayIntersection").gameObject.SetActive(false);
+                    transform.Find("3wayIntersection").gameObject.SetActive(false);
+                    transform.Find("curvedRoad2").gameObject.SetActive(false);
+                    if (directions.ContainsKey("north") | directions.ContainsKey("south"))
+                    {
+                        transform.Find("RoadObject").Rotate(0, 90f, 0, Space.Self);
+                    }
+                    break;
+                case 2:
+                    //Return either of the 2 way blocks
+                    transform.Find("4wayIntersection").gameObject.SetActive(false);
+                    transform.Find("3wayIntersection").gameObject.SetActive(false);
+                    transform.Find("curvedRoad2").gameObject.SetActive(false);
+                    transform.Find("RoadObject").gameObject.SetActive(false);
+                    if (directions.ContainsKey("north"))
+                    {
+                        if (directions.ContainsKey("south"))
+                        {
+                            transform.Find("RoadObject").gameObject.SetActive(true);
+                            transform.Find("RoadObject").Rotate(0, 90f, 0, Space.Self);
+                            break;
+                        }
+                        else if (directions.ContainsKey("east")) {
+                            transform.Find("curvedRoad2").gameObject.SetActive(true);
+                            transform.Find("curvedRoad2").Rotate(0, 270f, 0, Space.Self);
+                            break;
+                        }
+                        else {
+                            transform.Find("curvedRoad2").gameObject.SetActive(true);
+                            transform.Find("curvedRoad2").Rotate(0, 180f, 0, Space.Self);
+                            break;
+                        }
+
+
+                    }else if (directions.ContainsKey("east"))
+                    {
+                        if (directions.ContainsKey("west"))
+                        {
+                            transform.Find("RoadObject").gameObject.SetActive(true);
+                            break;
+
+                        }
+                        else
+                        {
+                            transform.Find("curvedRoad2").gameObject.SetActive(true);
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        transform.Find("curvedRoad2").gameObject.SetActive(true);
+                        transform.Find("curvedRoad2").Rotate(0, 90f, 0, Space.Self);
+                        break;
+                    }
+
+                    break;
+                case 3:
+                    //Return the 3 way block
+                    transform.Find("curvedRoad2").gameObject.SetActive(false);
+                    transform.Find("4wayIntersection").gameObject.SetActive(false);
+                    transform.Find("RoadObject").gameObject.SetActive(false);
+                    if (directions.ContainsKey("north"))
+                    {
+                        if (directions.ContainsKey("south"))
+                        {
+
+
+                            if (directions.ContainsKey("east"))
+                            {
+                                transform.Find("3wayIntersection").gameObject.SetActive(true);
+                                transform.Find("3wayIntersection").Rotate(0f, 270.0f, 0.0f, Space.Self);
+                            }
+                            else
+                            {
+                                transform.Find("3wayIntersection").gameObject.SetActive(true);
+                                transform.Find("3wayIntersection").Rotate(0f, 90.0f, 0.0f, Space.Self);
+                            }
+                        }
+                        else
+                        {
+                            transform.Find("3wayIntersection").gameObject.SetActive(true);
+                            transform.Find("3wayIntersection").Rotate(0f, 180.0f, 0.0f, Space.Self);
+                        }
+                    }
+                    else
+                    {
+                        transform.Find("3wayIntersection").gameObject.SetActive(true);
+
+                    }
+                    break;
+                case 4:
+                    //Return the 4 way intersection block
+                    transform.Find("curvedRoad2").gameObject.SetActive(false);
+                    transform.Find("3wayIntersection").gameObject.SetActive(false);
+                    transform.Find("RoadObject").gameObject.SetActive(false);
+                    transform.Find("4wayIntersection").gameObject.SetActive(true);
+                    
+
+                    break;
+                default:
+                    break;
 
             }
 
