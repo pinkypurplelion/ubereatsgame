d72fb379317284359f372f2d3302288c600a606f
Liam Angus <liam.angus@protonmail.com>
2022-10-02 13:58:08 +1100

    Updates to Upgrade UI System

--- Assets/Scenes/MainGameWorld/Scripts/PlayerUpgradeUIManager.cs
+++ Assets/Scenes/MainGameWorld/Scripts/PlayerUpgradeUIManager.cs
@@ -9,6 +9,10 @@ namespace Scenes.MainGameWorld.Scripts
     {
         private UIDocument _uiDocument;
         private VisualElement _rootVisualElement;
+
+        private ScrollView PlayerScroll;
+        private ScrollView VehicleScroll;
+        
         
         private Button ButtonCancel;
         private Button ButtonSave;
@@ -20,10 +24,13 @@ namespace Scenes.MainGameWorld.Scripts
             ButtonCancel = _rootVisualElement.Q<Button>("ButtonCancel");
             ButtonSave = _rootVisualElement.Q<Button>("ButtonSave");
             
+            PlayerScroll = _rootVisualElement.Q<ScrollView>("PlayerScroll");
+            VehicleScroll = _rootVisualElement.Q<ScrollView>("VehicleScroll");
+            
             ButtonCancel.RegisterCallback<ClickEvent>(ButtonCancelEvent);
             ButtonSave.RegisterCallback<ClickEvent>(ButtonSaveEvent);
         }
-        
+           
         private void ButtonCancelEvent(ClickEvent evt)
         {
             SceneManager.LoadScene("MainGameWorld");
@@ -39,10 +46,46 @@ namespace Scenes.MainGameWorld.Scripts
             Button button = evt.currentTarget as Button;
         }
 
+        private Box GenVehicleUpgrade(VehicleUpgrade upgrade)
+        {
+            Box vehicleBox = new Box();
+            
+            Label vehicleName = new Label();
+            Label vehicleCost = new Label();
+            Button purchaseButton = new Button();
+            
+            vehicleName.text = upgrade.name;
+            vehicleCost.text = upgrade.cost.ToString();
+            
+            if (upgrade.isPurchased)
+            {
+                purchaseButton.text = "Select Vehicle";
+            }
+            else
+            {
+                purchaseButton.text = "Purchase Vehicle";
+            }
+            purchaseButton.name = upgrade.vehicleID;
+            
+            vehicleBox.Add(vehicleName);
+            vehicleBox.Add(vehicleCost);
+            vehicleBox.Add(purchaseButton);
+            
+            return vehicleBox;
+        }
+        
         // Start is called before the first frame update
         void Start()
         {
-        
+            VehicleUpgrade demo = new VehicleUpgrade
+            {
+                name = "Demo",
+                cost = 100f,
+                isPurchased = false,
+                vehicleID = "Demo123"
+            };
+            
+            VehicleScroll.Add(GenVehicleUpgrade(demo));
         }
 
         // Update is called once per frame
---
+++ Assets/Scenes/MainGameWorld/Scripts/VehicleUpgrade.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace Scenes.MainGameWorld.Scripts
+{
+    [Serializable]
+    public class VehicleUpgrade
+    {
+        public static List<VehicleUpgrade> AllUpgrades = new List<VehicleUpgrade>();
+
+        public float cost;
+        public bool isPurchased;
+        public string name;
+        public string vehicleID;
+    }
+}
